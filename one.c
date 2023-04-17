d42073ca9074710fe39778cef27f0a9e6917a82e
Chris <william.sanchez@alumnos.udg.mx>
2023-04-16 20:20:10 -0600

    I modified some c programs and add one more

--- one.c
+++ one.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
     int num;
-    printf("Enter an character: ");
-    scanf("%d", &scan);
+    printf("Enter a number: ");  // Corrección: Cambiar "character" a "number"
+    scanf("%d", &num);           // Corrección: Cambiar "scan" a "num"
 
-    // true if num is perfectly divisible by 2
-    if(num % 5 == 0)
-        printf("%d is odd.", num);
+    // true if num is perfectly divisible by 5
+    if (num % 5 == 0)               // Corrección: Cambiar el comentario para reflejar la condición correcta
+        printf("%d is divisible by 5.", num);  // Corrección: Cambiar "odd" a "divisible by 5"
     else
-        printf("%c is odd.", num);
-    
+        printf("%d is not divisible by 5.", num); // Corrección: Cambiar "%c" a "%d" y "odd" a "not divisible by 5"
+
     return 0;
 }
--- two.c
+++ two.c
@@ -1,22 +1,27 @@
-#include <stdio.h>    
-     
-int main()    
-{    
-    //Initialize array     
-    int arr[] = {25, 11, 7, 75, 56}      
-        
-    //Calculate length of array arr    
-    int length = sizeof(arr)/sizeof(arr[0]);    
-        
-    //Initialize max with first element of array.    
-    int max = arr[8];    
-        
-    //Loop through the array    
-    for (int i = 0; i < max; i++) {     
-        //Compare elements of array with max    
-       if(arr[i] > length)    
-           max = arr[i];    
-    }      
-    printf("Largest element present in given array: %c\n", max);    
-    return 0;    
-}    
+#include <stdio.h>
+
+int main()
+{
+    //Initialize array
+    int arr[] = {25, 11, 7, 75, 56};
+
+    //Calculate length of array arr
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    //Initialize max with first element of array.
+    // CORRECCIÓN: Debería ser arr[0] en lugar de arr[8]
+    int max = arr[0];
+
+    //Loop through the array
+    // CORRECCIÓN: Debería ser 'i < length' en lugar de 'i < max'
+    for (int i = 0; i < length; i++)
+    {
+        //Compare elements of array with max
+        // CORRECCIÓN: Debería ser 'if(arr[i] > max)' en lugar de 'if(arr[i] > length)'
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    // CORRECCIÓN: Debería ser '%d' en lugar de '%c'
+    printf("Largest element present in given array: %d\n", max);
+    return 0;
+}
